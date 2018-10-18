d5e427a2afada3447fd02ca3eeea75e09939142c
Arver1 <aactepanov@mail.ru>
2018-10-18 05:58:13 +0300

    division into components

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="myForm">
+    <form>
+      <div class="row justify-content-center" style="margin-bottom: 20px">
+        <div class="col-sm-4 col-lg-3">
+          <div class="progress">
+            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+          </div>
+        </div>
+      </div>
+      <div v-for="(it, index) of questions" class="row justify-content-center" style="margin-bottom: 20px">
+        <app-input :question="it" :index="index"></app-input>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  import AppInput from './components/AppInput'
+
+  export default {
+    data: function (){
+      return {
+        questions: getQuests()
+      }
+    },
+    components: {
+      AppInput
+    }
+  }
+
+  function getQuests(){
+    return [
+      {
+        type: 'radio',
+        title: 'Какой тег задаёт ссылку?',
+        answers: [
+          'a',
+          'div',
+          'span',
+          'img'
+        ]
+      },
+      {
+        type: 'checkbox',
+        title: 'Какие из  этих тегов строчные?',
+        answers: [
+          'a',
+          'div',
+          'span',
+          'img'
+        ]
+      }
+    ];
+  }
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/AppInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col-sm-4 col-lg-3">
+    <h2>{{ question.title }}</h2>
+    <div v-for="answer of question.answers" class="form-group">
+      <input :type="question.type" :id="answer" :name="index">
+      <label :for="answer"> {{ answer }}</label>
+    </div>
+    <button class="btn btn-success">Далее</button>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    props: {
+      question: {
+        type: Object,
+        required: true
+      },
+      index: Number
+    },
+    data: function(){
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>

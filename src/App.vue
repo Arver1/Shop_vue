e5dc29760fac0ca526a06ea2ca0d68d012285a61
Arver1 <aactepanov@mail.ru>
2018-10-19 23:40:50 +0300

    add logic questions

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,21 @@
       <div class="row justify-content-center" style="margin-bottom: 20px">
         <div class="col-sm-4 col-lg-3">
           <div class="progress">
-            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+            <div class="progress-bar progress-bar-striped progress-bar-animated"
+                 role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"
+                 :style="{width: progressBar}"
+                 :class="progressBar === '100%' ? 'bg-success' : null"
+            ></div>
           </div>
         </div>
       </div>
-      <div v-for="(it, index) of questions" class="row justify-content-center" style="margin-bottom: 20px">
-        <app-input :question="it" :index="index"></app-input>
+      <div class="row justify-content-center" style="margin-bottom: 20px">
+        <template v-for="(it, index) of questions">
+          <app-input v-if="current === index"
+                     @app-next="current++"
+                     @app-valid="appValidCheck($event, index)" :question="it" :index="index">
+          </app-input>
+        </template>
       </div>
     </form>
   </div>
@@ -17,11 +26,31 @@
 
 <script>
   import AppInput from './components/AppInput'
+  const questions = getQuests();
+  const questionsLength = questions.length;
 
   export default {
     data: function (){
       return {
-        questions: getQuests()
+        questions,
+        fields: new Array(questionsLength).fill({
+          valid: false
+        }),
+        current: 0
+      }
+    },
+    computed: {
+      progressBar: function(){
+        let amount = 0;
+        this.fields.forEach((it) => {
+          if(it.valid) amount++;
+        });
+        return amount / questionsLength * 100 + '%';
+      }
+    },
+    methods: {
+      appValidCheck(e, index){
+        this.$set(this.fields, index, {valid: e})
       }
     },
     components: {
--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -2,10 +2,10 @@
   <div class="col-sm-4 col-lg-3">
     <h2>{{ question.title }}</h2>
     <div v-for="answer of question.answers" class="form-group">
-      <input :type="question.type" :id="answer" :name="index">
+      <input :type="question.type" :id="answer" :name="index" :value="answer" @change="checkValid">
       <label :for="answer"> {{ answer }}</label>
     </div>
-    <button class="btn btn-success">Далее</button>
+    <button class="btn btn-success" :disabled="!valid" @click.prevent="next">Далее</button>
   </div>
 </template>
 
@@ -21,7 +21,35 @@
     },
     data: function(){
       return {
-
+        valid: false
+      }
+    },
+    computed: {
+      value: function(){
+        if(this.question.type === 'checkbox'){
+          const obj = {};
+          this.question.answers.forEach((it) =>  obj[it] = false)
+          return obj;
+        }
+        return false
+      }
+    },
+    methods: {
+      checkValid: function(e){
+        if(this.question.type === 'checkbox'){
+          let flag = false;
+          this.value[e.target.value] = !this.value[e.target.value];
+          for(let it in this.value) {
+            if(this.value[it]) flag = true;
+          }
+          this.valid = flag;
+          return this.$emit('app-valid', flag)
+        }
+        this.valid = true;
+        return this.$emit('app-valid', true)
+      },
+      next: function(){
+        this.$emit('app-next')
       }
     }
   }
